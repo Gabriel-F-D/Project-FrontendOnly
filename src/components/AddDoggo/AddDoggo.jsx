3c621c36bffe967ae038d6c811d69539ea4646d3
Gabriel-F-D <gdelgadillo299@gmail.com>
2021-09-15 18:24:11 -0300

    front fixes sequel host

--- src/components/AddDoggo/AddDoggo.jsx
+++ src/components/AddDoggo/AddDoggo.jsx
@@ -17,7 +17,7 @@ function AddDoggo() {
 
     async function handleSubmit(e) {
         e.preventDefault();
-        const response = await fetch('http://localhost:3001/', {
+        const response = await fetch('https://project-doggo.herokuapp.com/', {
             method: "POST",
             headers: {
                 Accept: "application/json",
